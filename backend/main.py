df6cc0280237e3a5f3880bd4443d779c9310a384
Deepak singh Rathore <ds830140@gmail.com>
2026-07-07 18:23:02 +0530

    add CORS for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-powered-debt-relief-financial-re-ten.vercel.app",       
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
